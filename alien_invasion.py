44c0c29d8f403959accf9105b5a93a26a7087cc3
Kuntur Muenala <47254516+Kuntur-Mallku@users.noreply.github.com>
2025-07-24 00:00:49 -0500

    update alien moved

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,19 @@ class AlienInvasion:
 
         self._create_fleet()
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _create_fleet(self):
         """Create a full fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
@@ -55,6 +68,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -111,6 +125,11 @@ class AlienInvasion:
         self.aliens.draw(self.screen)
 
         pygame.display.flip()
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update the positions"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
